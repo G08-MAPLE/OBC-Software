68b4b0dc6ff60d69302e8d328a739bb74a4df46d
Brett Stevens <steven78@myumanitoba.ca>
2024-02-03 17:31:54 -0600

    starting heartbeat logic

--- include/heartbeat.hpp
+++ include/heartbeat.hpp
@@ -1,6 +1,15 @@
 #ifndef HEARTBEAT_H
 #define HEARTBEAT_H
 
+typedef enum {
+    CONN_NONE = 0,
+    CONN_LOST,
+    CONN_PRESENT
+} conn_state_t;
+
+#define MAX_MISSES 5;               //Num of consecutive misses definining lost connection
+#define HEARTBEAT_TASK_DELAY_MS 50; //Num of ms to wait before counting a loss
+
 void heartbeat(void * param);
 
 #endif
--- src/heartbeat.cpp
+++ src/heartbeat.cpp
@@ -1,7 +1,90 @@
+#include <Arduino.h>
+#include "freertos/task.h"
+#include "freertos/FreeRTOS.h"
 #include "heartbeat.hpp"
 
+static conn_state_t xbee_state;
+static int misses;
+
+int get_xbee_state(){
+    return xbee_state;
+}
+
+int get_misses(){
+    return misses;
+}
+
+void increment_misses(){
+    misses++;
+}
+
+void clear_misses(){
+    misses = 0;
+}
+
 void heartbeat(void * param){
+    BaseType_t retRTOS;
+    xbee_state = CONN_NONE;
+    misses = 0;
+
     for(;;){
-        
+		//Check if ground computer has sent a message
+		retRTOS = xTaskNotifyWait(0x00,0x00, NULL, HEARTBEAT_TASK_DELAY_MS);
+
+		//check if the ground station responded
+		if(retRTOS == pdTRUE){
+			//received notification
+
+			//reset misses counter
+			misses = 0;
+
+			//check if we have previously lost the connection
+			if(xbee_state == CONN_LOST){
+                //Log(XBEE: Reconnected)
+			}
+			else if(xbee_state == CONN_NONE){
+				//connecting with ground station for the first time
+                //Log(XBEE: Made connection with ground station)
+			}
+
+			//set state
+			xbee_state = CONN_PRESENT;
+		}
+		else{
+			//did not receive notification from ground computer
+			if(xbee_state == CONN_PRESENT){
+
+				if(misses < MAX_MISSES){
+					//losing ground computer
+					misses++;
+
+					//just for safety
+					xbee_state = CONN_PRESENT;
+				}
+				else{
+					//lost ground computer
+					xbee_state = CONN_LOST;
+					logMessage("Heartbeat: Lost Connection with ground computer", true);
+				}
+			}
+			else if(xbee_state == CONN_NONE ){
+				if(misses < MAX_MISSES){
+					//losing ground computer
+					misses++;
+
+					//just for safety
+					xbee_state = CONN_NONE;
+
+				}
+				else{
+					//lost ground computer
+					xbee_state = CONN_NONE;
+					logMessage("Heartbeat: Could not connect with ground computer", true);
+				}
+			}
+			else{
+				
+			}
+		}
     }
 }
