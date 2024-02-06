dc1064d2ffa6129c7fb77d5849de7d983b67abd2
bazins <svbazin9@gmail.com>
2024-02-06 16:55:04 -0600

    Solved issue that caused controller to revert to boot loop when startup task was called. The RTOS task was not given a large enough stack size on creation. Increased from 1000 to 3000.

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -5,47 +5,47 @@ void startTasks(){
     xTaskCreate(
         startup,      // Function that should be called
         "startup",   // Name of the task (for debugging)
-        1000,            // Stack size (bytes)
+        3000,            // Stack size (bytes)
         NULL,            // Parameter to pass
         1,               // Task priority
         NULL             // Task handle
     );
 
-    xTaskCreate(
-        XBEE_rx,      // Function that should be called
-        "XBEE_rx",   // Name of the task (for debugging)
-        1024*4,            // Stack size (bytes) (MAY NEED MORE BASED ON BUFFER SIZE)
-        NULL,            // Parameter to pass
-        configMAX_PRIORITIES,               // Task priority
-        NULL             // Task handle
-    );
+    // xTaskCreate(
+    //     XBEE_rx,      // Function that should be called
+    //     "XBEE_rx",   // Name of the task (for debugging)
+    //     1024*4,            // Stack size (bytes) (MAY NEED MORE BASED ON BUFFER SIZE)
+    //     NULL,            // Parameter to pass
+    //     configMAX_PRIORITIES,               // Task priority
+    //     NULL             // Task handle
+    // );
 
-    xTaskCreate(
-        heartbeat,      // Function that should be called
-        "heartbeat",   // Name of the task (for debugging)
-        1000,            // Stack size (bytes)
-        NULL,            // Parameter to pass
-        1,               // Task priority
-        NULL             // Task handle
-    );
+    // xTaskCreate(
+    //     heartbeat,      // Function that should be called
+    //     "heartbeat",   // Name of the task (for debugging)
+    //     1000,            // Stack size (bytes)
+    //     NULL,            // Parameter to pass
+    //     1,               // Task priority
+    //     NULL             // Task handle
+    // );
 
-    xTaskCreate(
-        burn_manager,      // Function that should be called
-        "burn manager",   // Name of the task (for debugging)
-        1000,            // Stack size (bytes)
-        NULL,            // Parameter to pass
-        1,               // Task priority
-        NULL             // Task handle
-    );
+    // xTaskCreate(
+    //     burn_manager,      // Function that should be called
+    //     "burn manager",   // Name of the task (for debugging)
+    //     1000,            // Stack size (bytes)
+    //     NULL,            // Parameter to pass
+    //     1,               // Task priority
+    //     NULL             // Task handle
+    // );
 
-    xTaskCreate(
-        systemSummary,      // Function that should be called
-        "systemSummary",   // Name of the task (for debugging)
-        1000,            // Stack size (bytes)
-        NULL,            // Parameter to pass
-        1,               // Task priority
-        NULL             // Task handle
-    );
+    // xTaskCreate(
+    //     systemSummary,      // Function that should be called
+    //     "systemSummary",   // Name of the task (for debugging)
+    //     1000,            // Stack size (bytes)
+    //     NULL,            // Parameter to pass
+    //     1,               // Task priority
+    //     NULL             // Task handle
+    // );
 
     // xTaskCreate(
     //     acc_read,      // Function that should be called
