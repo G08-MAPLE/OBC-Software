281b16c8b008feb07da61656e830988e241b9ba7
bazins <svbazin9@gmail.com>
2024-02-22 18:28:45 -0600

    Added gForceConversion function to acc.cpp so that we can log G force instead of raw ADC output. Tested by comparing raw ADC values to calculated Force, no issues found.

--- src/acc.cpp
+++ src/acc.cpp
@@ -15,14 +15,26 @@ void acc_read(void * param){
     ADC121C mADC121C = ADC121C(0x50);               // Address of ADC chip is 0x50, found experimentally
     mADC121C.begin();
     unsigned int conversion;
+    unsigned int measuredForce;
 
     for(;;){
         conversion = mADC121C.readConversion();
-        ESP_LOGI(ACC_TAG, "ADC: %d\n", conversion);
+        measuredForce = gForceConversion(conversion);
+        ESP_LOGI(ACC_TAG, "Measured Force: %d G's\n", measuredForce);
         
         // Add support to write to SD card
         // Made sure that write process is thread safe
 
         vTaskDelay(pdMS_TO_TICKS(10));              // Run at ~100 Hz
     }
+}
+
+unsigned int gForceConversion(unsigned int rawADCValue) {
+    // 12-bit ADC, max value = 4095
+    // Max G Force is 500, and is proportional to value read from ADC
+    const int maxGReading = 500;
+    const int maxADCReading = 4095;
+    unsigned int gForceValue = maxGReading*rawADCValue/maxADCReading;
+
+    return gForceValue;
 }
